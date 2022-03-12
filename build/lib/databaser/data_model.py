f6f5893d84dc39be5ec56b061b67b7c9fc50b52b
Aly Hassan <alyhassan10@hotmail.com>
2022-03-12 06:05:52 +0200

    UPDATE: Enable schema name in constructor

--- build/lib/databaser/data_model.py
+++ build/lib/databaser/data_model.py
@@ -1,5 +1,7 @@
 from typing import List
 
+import function as function
+
 from databaser.engine.engine import DatabaseEngine
 from databaser.parser.table_structure.create_table import TableField
 from databaser.pgsql import Query
@@ -9,11 +11,11 @@ from config import conn_string
 
 class DataModel:
     table_name = None
-    schema_name = 'public'
     fields: List[TableField] = []
 
-    def __init__(self):
+    def __init__(self, schema_name: str = "public"):
         self.__sql = []
+        self.schema_name = schema_name
 
     def __get_fields(self):
         fields = []
@@ -34,10 +36,10 @@ class DataModel:
         self.__sql.append(sql)
         return self
 
-    def get_one(self, fields: List[str] = None, condition: dict = {}, joins: dict = {}):
+    def get_one(self, fields: List[str] = None, condition: dict = {}, joins: dict = {}, order_by: dict = {}):
         sql = Query(
             'pgsql'
-        ).find(self.table_name, fields, condition, joins, limit=1, schema_name=self.schema_name).get_sql()
+        ).find(self.table_name, fields, condition, joins, limit=1, schema_name=self.schema_name, order_by=order_by).get_sql()
         # print("This instance:", id(self))
         # print("This instance:", self.__sql)
 
@@ -46,6 +48,8 @@ class DataModel:
         return self
 
     def insert(self, data: dict, value_quote: bool = True):
+        self.on_insert()
+
         sql = Query(
             'pgsql'
         ).insert(self.table_name, data=data, value_quote=value_quote, schema_name=self.schema_name).get_sql()
@@ -53,9 +57,20 @@ class DataModel:
         return self
 
     def update(self, data: dict, conditions: dict, value_quote: bool = True):
+        self.on_update()
+
         sql = Query(
             'pgsql'
         ).update(self.table_name, data=data, conditions=conditions, value_quote=value_quote, schema_name=self.schema_name).get_sql()
+        self.__sql.append(sql)
+        return self
+
+    def delete(self, data: dict, conditions: dict, value_quote: bool = True):
+        self.on_delete()
+
+        sql = Query(
+            'pgsql'
+        ).delete(self.table_name, conditions=conditions, value_quote=value_quote, schema_name=self.schema_name).get_sql()
         self.__sql.append(sql)
         return self
 
@@ -66,7 +81,9 @@ class DataModel:
         return DatabaseEngine(**conn_string).execute(self.__sql, has_return=True, return_many=(not fetch_one))
 
     def commit(self):
-        return DatabaseEngine(**conn_string).execute(self.__sql, transaction=True)
+        res = DatabaseEngine(**conn_string).execute(self.__sql, transaction=True)
+        self.on_commit()
+        return res
 
     def set_schema_name(self, schema_name: str = "public"):
         self.schema_name = schema_name
@@ -74,3 +91,23 @@ class DataModel:
 
     def get_schema_name(self) -> str:
         return self.schema_name
+
+    def on_insert(self, func: function = None):
+        if func:
+            func()
+        return self
+
+    def on_update(self, func: function = None):
+        if func:
+            func()
+        return self
+
+    def on_commit(self, func: function = None):
+        if func:
+            func()
+        return self
+
+    def on_delete(self, func: function = None):
+        if func:
+            func()
+        return self
